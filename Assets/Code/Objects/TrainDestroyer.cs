1ea606fc2f1aa60b14fb17e0415aa7a650ff8a4e
gevrest <149320260+gevrest@users.noreply.github.com>
2026-01-02 00:22:26 +0500

    New features and fixes
    - added disappearance of the train
    - fixed flashlight shadows
    - fixed raycast interaction
    - prepared to baking lighting

---
+++ Assets/Code/Objects/TrainDestroyer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    [RequireComponent (typeof (Collider))]
+    public class TrainDestroyer : MonoBehaviour
+    {
+        [SerializeField] private GameObject _train;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                Destroy(_train);
+            }
+        }
+    }
+}
--- Assets/Code/Player/RaycastInteractor.cs
+++ Assets/Code/Player/RaycastInteractor.cs
@@ -5,29 +5,28 @@ namespace Game
     public sealed class RaycastInteractor : MonoBehaviour
     {
         [SerializeField] private Transform _rayPoint;
+        [SerializeField] private float _maxDistance = 2f;
 
         private InteractionIndicator _interactionIndicator;
 
         private void Start()
         {
             _interactionIndicator = FindAnyObjectByType<InteractionIndicator>();
+            _interactionIndicator.Deactivate();
         }
 
         private void Update()
         {
-            if (Physics.Raycast(_rayPoint.position, _rayPoint.forward, out var hitInfo))
+            if (Physics.Raycast(_rayPoint.position, _rayPoint.forward, out var hitInfo, _maxDistance) && hitInfo.collider.TryGetComponent(out InteractableObject interactableObject))
             {
-                if (hitInfo.collider.TryGetComponent(out InteractableObject interactableObject))
-                {
-                    _interactionIndicator.Activate();
+                _interactionIndicator.Activate();
 
-                    if (Input.GetKeyDown(KeyCode.E))
-                        interactableObject.Interact();
-                }
-                else
-                {
-                    _interactionIndicator.Deactivate();
-                }
+                if (Input.GetKeyDown(KeyCode.E))
+                    interactableObject.Interact();
+            }
+            else
+            {
+                _interactionIndicator.Deactivate();
             }
         }
     }
